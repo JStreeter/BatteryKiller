0e5ca14ac90e73f5212af36dcc3bb0ce4c8715b3
kz5ee <kz5ee@yahoo.com>
2013-07-28 20:02:19 -0500

    Added UI files.

---
+++ user_interface.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   user_interface.h
+ * Author: Mike
+ *
+ * Created on July 28, 2013, 7:03 PM
+ */
+
+#ifndef USER_INTERFACE_H
+#define	USER_INTERFACE_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* USER_INTERFACE_H */
+
